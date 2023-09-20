503fe5c319253eb3694f231d89eb7ed744a3e0c3
fob-Ji <cycyou@naver.com>
2023-09-21 05:23:54 +0900

    adding solutions of twenty-seventh week

--- Twenty_seventh_week/bj14261_mosol.py
+++ Twenty_seventh_week/bj14261_mosol.py
@@ -14,7 +14,10 @@ def solution():
 
     n, m = map(int, input().split())
     genders = list(input().split())
-    parent = []
+    parent = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        parent[i] = i
 
     edges = []
     for _ in range(m):
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from NineteenthWeek import ex01_search_parts
 from TwelfthWeek import q02_mulOrPlus, bj_sugarDelivery
 from TwentiethWeek import bj24041_sungsimdang
 from Twenty_fifth_week import bj4485_The_one_in_green_is_Zelda_right
+from Twenty_seventh_week import bj14261_mosol
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -87,5 +88,6 @@ if __name__ == '__main__':
     # prac26_card_sort.solution()
     # ex01_search_parts.solution()
     # bj24041_sungsimdang.solution()
-    bj4485_The_one_in_green_is_Zelda_right.solution()
+    # bj4485_The_one_in_green_is_Zelda_right.solution()
+    bj14261_mosol.solution()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
